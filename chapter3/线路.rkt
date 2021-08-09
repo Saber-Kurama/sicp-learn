c35e29e53170b1911bca9514c85a9634dc08f99d
guochao <463561248@qq.com>
2021-08-09 21:24:47 +0800

    线路的代码

--- chapter3/线路.rkt
+++ chapter3/线路.rkt
@@ -1,5 +1,7 @@
 #lang sicp
 
+
+
 (define (call-each s)
     'done
 )
@@ -19,7 +21,7 @@
         )
         (define (dispatch m)
             (cond ((eq? m `get-signal) signal-value)
-                  ((eq? m `set-my-signal!) set-my-signal!)
+                  ((eq? m `set-signal!) set-my-signal!)
                   ((eq? m `add-action!) accept=action-procedure!)
                   (else (error "Unknow operaction" m))
             )
@@ -27,3 +29,47 @@
         dispatch
     )
 )
+
+; 一些基本操作
+; 基本功能模块
+; 获取线的值
+(define (get-signal wire)
+    (wire 'get-signal)
+)
+; 设置线的值 （会执行 action）
+(define (set-signal! wire value)
+    ((wire 'set-signal!) value)
+)
+; 线添加一些事件
+(define (add-action! wire action-procedure)
+    ((wire 'add-action!) action-procedure)
+)
+; atfer-delay 延迟一段时间 执行 一个过程
+
+(define (after-delay delay action)
+    (add-to-agenda! (+ delay (current-time the-agenda))
+                    action
+                    the-agenda))
+
+; 返回新建的空待处理表
+(make-agenda) 
+; 判断待处理表是否为空
+(empty-agenda? <agenda>) 
+; 返回待处理表中第一个项
+(first-agenda-item <agenda>) 
+; 删除待处理表里的第一项
+(remove-first-agenda-item! <agenda>) 
+; 向待处理表中加入一 项，其意义是要求在给定时间运行的过程
+(add-to-agenda! <time> <action> <agenda>) 
+; 返回当前时间
+(current-time <agenda>) 
+
+(define (probe name wire)
+    (add-action! wire
+                 (lambda ()        
+                    (newline)
+                    (display name)
+                    (display " ")
+                    (display (current-time the-agenda))
+                    (display "  New-value = ")
+                    (display (get-signal wire)))))
